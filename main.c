058cabe601df9e160828dadd2dbcdaecff5d2f07
vladislav1q <30519764+vladislav1q@users.noreply.github.com>
2018-09-15 00:15:15 +0300

    Split H/w from 13.09.18

--- main.c
+++ main.c
@@ -1,29 +1,56 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
-void GenerateString(int n, char* string){
-    if(n < 1)
-        return;
-    string[0] = 'a';
-    char str[1];
-    for(int i = 1; i < n; i++){
-        str[0] = (char)('a'+i);
-        strcat(string, str);
-        //printf("ERROR1\n");
-        strncpy(string + (1<<i), string, (1<<i)-1);
+void Split(char* str, char* delimeters, char** words, int* count){
+    int res = 0;
+    for(int i = 0, a = 0; i < 3000, *count < 100, a < 30; i++) {
+        if (str[i] == '\0')
+            break;
+        for(int t = 0; t < 10; t++){
+            if((int)str[i] == (int)delimeters[t]){
+                res++;
+                break;
+            }
+        }
+        if(res == 0){
+            words[*count][a] = str[i];
+            a++;
+        } else if(a > 0){
+            (*count)++;
+            a = 0;
+        }
+        res = 0;
     }
 }
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    char* a = calloc((1 << n), sizeof(char));
+            //GIVE ALL MEMORY
+    char delimeters[10] = {' ', ',', '.', ';', ':', '(', ')', '!', '\'', '\"',};
+    char* str = calloc(4000, sizeof(char));
+    char** words = calloc(100, sizeof(char*));
+    for(int i = 0; i < 100; i++){
+        words[i] = calloc(30, sizeof(char));
+    }
+    int count = 0;
 
-    GenerateString(n, a);
+    FILE* f;
+    if((f = fopen("/home/vladislav/CLionProjects/split/check.txt", "a+")) == NULL)
+        printf("There is no this file!\n");
+    fgets(str, 4000, f);
 
-    printf("%s\n", a);
+    Split(str, delimeters, words, &count);
+    printf("Count of words: %d\n", count);
+    for(int i = 0; i < count; i++){
+        printf("\"%s\" ", words[i]);
+    }
+    printf("\n");
 
-    free(a);
+            //FREE ALL MEMORY
+    for(int i = 0; i < 100; i++){
+        free(words[i]);
+    }
+    free(words);
+
     return 0;
 }
